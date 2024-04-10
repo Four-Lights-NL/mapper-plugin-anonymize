0c101ca65670a4f37450aeb735605c941780908d
Thomas Rijpstra <thomas@fourlights.nl>
2024-04-10 08:34:08 +0200

    feat: add configuration for anonymization methods

--- src/lib/anonymize.test.ts
+++ src/lib/anonymize.test.ts
@@ -2,6 +2,7 @@ import { name as packageName } from '#package.json'
 import { map, MapperConfig } from '@fourlights/mapper'
 import AnonymizePlugin from './anonymize'
 import { AnonymizePluginPropertyOptions } from './types'
+import { FakeMethodOptions } from './methods/fake'
 
 describe(packageName, () => {
 	it('should not anonymize properties without classification', () => {
@@ -19,10 +20,10 @@ describe(packageName, () => {
 		const plugin = new AnonymizePlugin({ seed: 1, piiData: 'redact' })
 		const input = { email: 'exposed@example.com' }
 		const expected = { email: '*****@*****.com' }
-		const config = {
+		const config: MapperConfig<typeof input, AnonymizePluginPropertyOptions> = {
 			email: {
 				value: (d) => d.email,
-				options: { classification: 'pii' } as AnonymizePluginPropertyOptions,
+				options: { classification: 'pii' },
 			},
 		} as MapperConfig<typeof input>
 
@@ -32,14 +33,92 @@ describe(packageName, () => {
 	it('should fake properties with a classification', () => {
 		const plugin = new AnonymizePlugin({ seed: 1, piiData: 'fake' })
 		const input = { email: 'exposed@example.com' }
-		const expected = { email: 'Winifred.Watsica99@gmail.com' }
-		const config = {
+		const expected = { email: 'Winifred.Watsica@gmail.com' }
+		const config: MapperConfig<typeof input, AnonymizePluginPropertyOptions> = {
 			email: {
 				value: (d) => d.email,
-				options: { classification: 'pii' } as AnonymizePluginPropertyOptions,
+				options: { classification: 'pii' },
 			},
 		} as MapperConfig<typeof input>
 
 		expect(map(input, config, { plugins: [plugin] })).toEqual(expected)
 	})
+
+	it('should allow customizing the faker', () => {
+		const plugin = new AnonymizePlugin({ seed: 1 })
+		const input = { firstName: 'Jane', lastName: 'Doe' }
+		const expected = { name: 'Winifred Watsica' }
+		const config: MapperConfig<typeof input, AnonymizePluginPropertyOptions> = {
+			name: {
+				value: (d) => `${d.firstName} ${d.lastName}`,
+				options: {
+					classification: 'pii',
+					anonymize: { method: 'fake', options: { key: 'fullName' } as FakeMethodOptions },
+				},
+			},
+		} as MapperConfig<typeof input>
+
+		expect(map(input, config, { plugins: [plugin] })).toEqual(expected)
+	})
+
+	// it('should allow custom anonymization methods', () => {
+	//   const plugin = new AnonymizePlugin({
+	//     piiData: (key, property) => {
+	//       if (property.options?.anonymize === 'fake') {
+	//         return (d) => `fake-${d[key]}`
+	//       }
+	//       return (d) => `redacted-${d[key]}`
+	//     },
+	//   })
+	// })
+
+	// it('should allow specifying anonymization methods per property', () => {
+	//   const plugin = new AnonymizePlugin({
+	//     piiData: {
+	//       method: 'fake',
+	//       options: { key: 'email' },
+	//     },
+	//   })
+	// })
+
+	// it('should allow specifying anonymization methods per property with a custom method', () => {
+	//   const plugin = new AnonymizePlugin({
+	//     piiData: {
+	//       method: (key, property) => {
+	//         if (property.options?.anonymize === 'fake') {
+	//           return (d) => `fake-${d[key]}`
+	//         }
+	//         return (d) => `redacted-${d[key]}`
+	//       },
+	//     },
+	//   })
+	// })
+
+	// it('should allow specifying anonymization methods per property with a custom method and options', () => {
+	// const plugin = new AnonymizePlugin({
+	//     piiData: {
+	//       method: (key, property) => {
+	//         if (property.options?.anonymize === 'fake') {
+	//           return (d) => `fake-${d[key]}`
+	//         }
+	//         return (d) => `redacted-${d[key]}`
+	//       },
+	//       options: { key: 'email' },
+	//     },
+	//   })
+	// })
+
+	// it('should allow specifying anonymization methods per property with a custom method and options', () => {
+	//   const plugin = new AnonymizePlugin({
+	//     piiData: {
+	//       method: (key, property) => {
+	//         if (property.options?.anonymize === 'fake') {
+	//           return (d) => `fake-${d[key]}`
+	//         }
+	//         return (d) => `redacted-${d[key]}`
+	//       },
+	//       options: { key: 'email' },
+	//     },
+	//   })
+	// })
 })
