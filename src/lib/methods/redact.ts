129b168b53befe05ee6cb422c02a2403650bea67
Thomas Rijpstra <thomas@fourlights.nl>
2024-04-12 11:14:10 +0200

    feat: add `traverse` option to allow applying anonymization to objects

--- src/lib/methods/redact.ts
+++ src/lib/methods/redact.ts
@@ -2,21 +2,23 @@ import type { MapperFn, MapperProperty } from '@fourlights/mapper'
 import type { AnonymizeMethodFactory } from '../types'
 import getMethodOptions from '../utils/getMethodOptions'
 import fuzzysort from 'fuzzysort'
+import unwrapValue from '../utils/unwrapValue'
 
 export type RedactMethodOptions = {
 	key?: string
 	replaceValue?: string
+	traverse?: boolean
 }
 
 class Redact<T> implements AnonymizeMethodFactory<T> {
-	private readonly specialRedactMethods: {
+	private readonly redactMethods: {
 		name: string
 		methodFactory: (replaceValue: string) => MapperFn<T>
 	}[] = []
 	private readonly minMatchKeyLength = 2
 
 	constructor() {
-		this.specialRedactMethods = [
+		this.redactMethods = [
 			{
 				name: 'email',
 				methodFactory: (replaceValue) => () =>
@@ -25,13 +27,39 @@ class Redact<T> implements AnonymizeMethodFactory<T> {
 		]
 	}
 
+	private shouldTraverse(property: MapperProperty<T>) {
+		const options = getMethodOptions<RedactMethodOptions>(property)
+		return options?.traverse ?? true
+	}
+
+	anonymize(key: string, property: MapperProperty<T>) {
+		const anonymizedProperty: MapperProperty<T> = {
+			value: (data: T, _wrappedKey?: string, rowId?: string | number) => {
+				if (typeof data[key as keyof T] === 'object' && this.shouldTraverse(property))
+					return property.value(data, key, rowId)
+				return this.generate(key, property)(data, key, rowId)
+			},
+		}
+
+		return this.shouldTraverse(property)
+			? ({
+					...anonymizedProperty,
+					row: (row, parentKey, rowId) => {
+						return this.generate(`${rowId!}`, property)(row, parentKey, rowId)
+					},
+				} as MapperProperty<T>)
+			: anonymizedProperty
+	}
+
 	generate(key: string, property: MapperProperty<T>) {
 		const options = getMethodOptions<RedactMethodOptions>(property)
+
 		const replaceValue = options?.replaceValue || '*'
-		const result = fuzzysort.go(key, this.specialRedactMethods, { key: 'name', limit: 1 })
+		const result = fuzzysort.go(key, this.redactMethods, { key: 'name', limit: 1 })
 		if (key.length < this.minMatchKeyLength || result.length === 0)
-			return (d: T) => `${property.value(d)}`.replaceAll(/./g, replaceValue)
-		return result[0].obj.methodFactory(replaceValue)
+			return (d: T, _wrappedKey?: string, rowId?: string | number) =>
+				`${unwrapValue(property, d, rowId)}`.replaceAll(/\w/g, replaceValue)
+		return (d: T) => result[0].obj.methodFactory(replaceValue)(d)
 	}
 }
 
